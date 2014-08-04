50e8f16d511eed1262e0badd5eb8e4edad6d4466
Andrew J Wright <the.concept.blend@gmail.com>
2014-08-03 23:37:59 -0600

    Added command line and better URL checking

--- README.md
+++ README.md
@@ -5,34 +5,39 @@ A basic web-crawler for auditing websites.
 
 ## Usage
 
-To crawl CAPP.ca:
+To crawl all pages on _mysite.com_ – collecting links within the body – and output the results to _mysite.tsv_:
 
 ```
-node capp_ca.js
+node main.js mysite.com '*' http://mysite.com/ body > mysite.tsv
 ```
 
-To crawl CAPP.ca/rce
+To crawl the _blog_ section on _mysite.com_ – collecting links within the div#main – and output the results to _mysite.tsv_:
 
 ```
-node rce.js
+node main.js mysite.com '/blog/*' http://mysite.com/blog/ '#main' > mysite.tsv
 ```
 
-To crawl others, duplicate capp_ca.js and adjust these parameters:
+### To create a reusable script:
+No one likes typing out long sentences to the command line. Here is _mysite.sh_:
 
-```
-var container_selector	= ".capp-Container",
-	output_separator	= "\t",
-	g_domain			= "www.capp.ca",
-	g_scope				= "*",
-	g_startPage			= "http://www.capp.ca/Pages/SiteMap.aspx";
+```sh
+#!/bin/sh
+if [ $1 ]; then
+	echo "Beginning crawl..."
+	node main.js mysite.com '/blog/*' http://mysite.com/blog/ '#main' > mysite.tsv
+	echo "Done."
+else
+	echo "Please specify an output file (e.g. myCrawl.tsv)."
+fi
 ```
 
+Don't forget to ```chmod u+x mysite.sh``` on your script file.
 
 ## To Do
 
-1. Accept command-line parameters.
+1. ~~Accept command-line parameters.~~	
 2. Write results directly to a file.
-2. Add better error checking for URLs.
+2. ~~Add better error checking for URLs.~~
 3. Capture the Response Code (200, 301, 404, etc.).
 4. Consider modifying the UserAgent string to represent this crawler.
 5. Add progress indicator.
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var spider = require('spider');
 
-var container_selector	= ".capp-Container",
-	output_separator	= "\t",
-	g_domain			= "www.capp.ca",
-	g_scope				= "*",
-	g_startPage			= "http://www.capp.ca/Pages/SiteMap.aspx";
-	
+var output_separator	= "\t",
+	g_domain			= process.argv[2] || "localhost:8888",
+	g_scope				= process.argv[3] || "*",
+	g_startPage			= process.argv[4] || "http://localhost:8888/index.html",
+	container_selector	= process.argv[5] || "body";
+
 // ==========================
 //	MemoryCache object
 // ==========================
@@ -13,14 +13,17 @@ var container_selector	= ".capp-Container",
 function MemoryCache () {
   this.cache = {};
 }
-MemoryCache.prototype.get = function (url, cb) {
+MemoryCache.prototype.get = function (_url, cb) {
+  var url = _url.toLowerCase();
   if (!this.cache[url]) return cb(null);
   cb({headers:this.cache[url].headers, body:this.cache[url].body.toString()});
 }
-MemoryCache.prototype.set = function (url, headers, body) {
+MemoryCache.prototype.set = function (_url, headers, body) {
+  var url = _url.toLowerCase();
   this.cache[url] = {headers:headers, body:new Buffer(body)};
 }
-MemoryCache.prototype.getHeaders = function (url, cb) {
+MemoryCache.prototype.getHeaders = function (_url, cb) {
+  var url = _url.toLowerCase();
   if (!this.cache[url]) return cb(null);
   cb(this.cache[url].headers);
 }
@@ -31,12 +34,14 @@ var cache = new MemoryCache();
 //	/MemoryCache object
 // ==========================
 
+console.log("Excludes feedback links and non-html files.")
+
 // Output some column headings
 var headings = ["URL", "Title", "Title Length", "Description", "Description Length", "Keywords", "Keywords Count", "H1", "H1 Length"];
 console.log( headings.join(output_separator));
 
 spider({
-	maxConnections: 6,
+	maxConnections: 12,
 	cache: cache
 })
 .route( g_domain, g_scope, function (window, $) {
@@ -46,11 +51,12 @@ spider({
 	$(container_selector + ' a').filter(function(i){
 		var self = $(this),
 			href = self.prop('href');
-		//return !!href && !!(href.search(/^mailto|javascript|tel/gi)==-1);
-		return !!href && (href.indexOf('http') == 0);
+		return !!href && (href.length > 0) && !!(href.search(/^mailto|javascript|tel/gi) == -1) && !!(href.search(/referrer/gi) == -1);
+		//return !!href && (href.length > 0) && !!(href.search(/^mailto|javascript|tel/gi) == -1) && !!(href.indexOf("getdoc.aspx") == -1);
+		//return !!href && (href.indexOf('http') == 0);
 	}).spider();
 	
-	
+	//totalPages++;
 	var output = [],
 		el_pageTitle = $('head > title'),
 		el_description = $('head > meta[name=description]'),
@@ -83,14 +89,16 @@ spider({
 	output.push( !!keywords_content ? keywords_content.split(',').length : 0 );
 	
 	// H1
-	output.push( !!h1 ? h1.text() : "" );
+	output.push( !!h1 ? h1.eq(0).text() : "" );
 	
 	// H1 length
-	output.push( !!h1 ? h1.text().length : 0 );
+	output.push( !!h1 ? h1.eq(0).text().length : 0 );
 	
 	console.log( output.join(output_separator) );
 	
 })
 .get( g_startPage )
-.log( 'info' )
+.log( 'log' )
 ;
+
+//console.log("Total Pages: "+totalPages);
